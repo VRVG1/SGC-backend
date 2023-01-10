00f158c4ed0bb89d5acaee9b3dad3cc1d4813294
Doncamatic1117 <dr.goku45@gmail.com>
2023-01-09 20:03:40 -0600

    - Aplicados los cambios de la nueva DB a modelos y Serializers. (Las Views las subo ya que se prueben)

--- SGC/materias/models.py
+++ SGC/materias/models.py
@@ -1,9 +1,6 @@
-from pyexpat import model
 from django.db import models
 from usuarios.models import Usuarios
 from django.core.validators import MaxValueValidator, MinValueValidator
-# Create your models here.
-
 
 class Carreras(models.Model):
     class Meta:
@@ -18,9 +15,14 @@ class Carreras(models.Model):
 class Materias(models.Model):
     class Meta:
         db_table = 'Materias'
-    ID_Materia = models.CharField(max_length=8, null=False, primary_key=True)
-    Nombre_Materia = models.CharField(max_length=200, null=False, unique=True)
-    Carrera = models.CharField(null=True, max_length=1)
+    Clave_reticula = models.CharField(max_length=8, null=False, primary_key=True)
+    Carrera = models.ForeignKey(Carreras, on_delete=models.CASCADE)
+    Nombre_Materia = models.CharField(max_length=200, null=False, unique=False)
+    horas_Teoricas = models.IntegerField(null=False, default=1, validators=[MinValueValidator(1),MaxValueValidator(10)])
+    horas_Practicas = models.IntegerField(null=False, default=1, validators=[MinValueValidator(1),MaxValueValidator(10)])
+    creditos = models.IntegerField(null=False, default=1, validators=[MinValueValidator(1),MaxValueValidator(20)])
+    unidades = models.IntegerField(null=False, default=1, validators=[MinValueValidator(1),MaxValueValidator(10)])
+
 
     def __str__(self) -> str:
         return f"{self.Nombre_Materia}"
@@ -40,13 +42,38 @@ class Asignan(models.Model):
         ('J', 'J'),
     )
 
+    horas = (
+        ('07:00 - 08:00','07:00 - 08:00'),
+        ('08:00 - 09:00','08:00 - 09:00'),
+        ('09:00 - 10:00','09:00 - 10:00'),
+        ('10:00 - 11:00','10:00 - 11:00'),
+        ('11:00 - 12:00','11:00 - 12:00'),
+        ('12:00 - 13:00','12:00 - 13:00'),
+        ('13:00 - 14:00','13:00 - 14:00'),
+        ('14:00 - 15:00','14:00 - 15:00'),
+        ('07:00 - 09:00','07:00 - 09:00'),
+        ('09:00 - 11:00','09:00 - 11:00'),
+        ('11:00 - 13:00','11:00 - 13:00'),
+        ('13:00 - 15:00','13:00 - 15:00'),
+    )
+
+    dias = (
+        ('Lunes','Lunes'),
+        ('Martes','Martes'),
+        ('Miercoles','Miercoles'),
+        ('Jueves','Jueves'),
+        ('Viernes','Viernes'),
+    )
+
     class Meta:
         db_table = 'Asignan'
     ID_Asignan = models.AutoField(primary_key=True, null=False)
-    ID_Materia = models.ForeignKey(Materias, on_delete=models.CASCADE)
     ID_Usuario = models.ForeignKey(Usuarios, on_delete=models.CASCADE)
-    ID_Carrera = models.ForeignKey(Carreras, on_delete=models.CASCADE)
-    Grado = models.IntegerField(null=False, default=1, validators=[
+    ID_Materia = models.ForeignKey(Materias, on_delete=models.CASCADE)
+    Semestre = models.IntegerField(null=False, default=1, validators=[
                                 MinValueValidator(1), MaxValueValidator(13)])
     Grupo = models.CharField(max_length=1, null=False,
                              choices=grupos, default='A')
+    Hora = models.CharField(max_length=14,null=False,choices=horas,default="--:-- - --:--")
+    Dia = models.CharField(max_length=10,null=False,choices=dias,default="Dia")
+    Aula = models.CharField(max_length=4,null=False,default='A00')
