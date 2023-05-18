ad9ef158f8be3698b21ea52f19021cd84bcdc766
Pedro Nicolas Rios Vargas <pv123124@gmail.com>
2023-05-18 01:12:22 -0600

    Reorganización de los validadores
    Fue deconstruida la estructura de la función validarDatos para poder
    reutilizar las validaciones de las estructuras de PNC en las
    validaciones de modificarPNC y eliminarPNC

--- SGC/reportes/pnc_validators.py
+++ SGC/reportes/pnc_validators.py
@@ -2,7 +2,8 @@ from rest_framework.response import Response
 from rest_framework import status
 import re
 
-def validarDatos(datos, registro_gral_pnc):
+
+def checkReceivedData(datos):
     if type(datos) is not dict:
         return Response(data={"Error": "Estructura de datos no valida."},
                         status=status.HTTP_400_BAD_REQUEST)
@@ -12,14 +13,6 @@ def validarDatos(datos, registro_gral_pnc):
         "lastPNCID": {"dataClass": int},
         "registro": {"dataClass": dict}
     }
-    atributos_pnc = {
-            "numeroPNC": {"dataClass": int},
-            "folio": {"dataClass": str},
-            "fechaRegistro": {"dataClass": str},
-            "especIncumplida": {"dataClass": str},
-            "accionImplantada": {"dataClass": str},
-            "isEliminaPNC": {"dataClass": bool}
-            }
 
     if len(datos_keys) != 2:
         return Response(data={"Error": "Estructura de datos no valida."},
@@ -40,9 +33,137 @@ def validarDatos(datos, registro_gral_pnc):
                 },
                 status=status.HTTP_400_BAD_REQUEST)
 
+    # Se crean las variables solo para dar contexto de lo que retornan
     lastPNCID = datos["lastPNCID"]
     registro = datos["registro"]
+    return (lastPNCID, registro)
 
+
+def checkReportesRegistrados(reportes_ids,
+                             reportes_registrados,
+                             registro_gral_idsReportes):
+    keys = reportes_registrados.keys()
+    if len(keys) != 1:
+        return Response(data={
+            "Error": "Estructura de reportesRegistrados no valida. Más elementos de los esperados"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    if "idsReportes" not in keys:
+        return Response(data={
+            "Error": "Estructura de reportesRegistrados no valida. No existe el elemento 'idsReportes'"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    new_ids_reportes = reportes_registrados["idsReportes"]
+    if type(new_ids_reportes) is not list:
+        return Response(data={
+            "Error": "Estructura de reportesRegistrados no valida. Valor relacionado a 'idsReportes' no es de tipo 'arreglo'"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    # El ultimo id agregado en idsReportes debe ser el del nuevo reporte
+    if new_ids_reportes[-1] not in reportes_ids:
+        return Response(data={
+            "Error": "Estructura de reportesRegistrados no valida. Id del nuevo reporte no concuerda"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    # Todos los ids dentro de idsReportes menos el ultimo son de reportes
+    # existentes
+    new_ids_reportes = new_ids_reportes[:-1]
+    # Comprobando si los ids pasados por parametro realmente existen en el registro
+    for id_reporte in new_ids_reportes:
+        if id_reporte in registro_gral_idsReportes:
+            return Response(data={
+                "Error": f"Estructura de reportesRegistrados no valida. Id '{id_reporte}' no existe en el servidor"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
+    return True
+
+
+def checkReporte(pnc_id, reporte, registro_gral):
+    new_registro_reporte_key = reporte.keys()
+    if len(new_registro_reporte_key) != 1:
+        return Response(data={
+            "Error": "Estructura de reporte_<ID> no valida. Más elementos de los esperados"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    if "linkedPNCs" not in new_registro_reporte_key:
+        return Response(data={
+            "Error": "Estructura de reporte_<ID> no valida. No existe el elemento 'linkedPNCs'"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    new_linked_pncs = reporte["linkedPNCs"]
+    if type(new_linked_pncs) is not list:
+        return Response(data={
+            "Error": "Estructura de reporte_<ID> no valida. Valor relacionado a 'linkedPNCs' no es de tipo 'arreglo'"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    # El último id agregado en linkedPNCs debe ser el nuevo pnc
+    if pnc_id != new_linked_pncs[-1]:
+        return Response(data={
+            "Error": "Estructura de reporte_<ID> no valida. Id del nuevo PNC no concuerda"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    # Todos los ids dentro de linkedPNCs menos el ultimo son de pncs existentes
+    new_linked_pncs = new_linked_pncs[:-1]
+    # Comprobando si los ids pasados por parametro realmente existen en el registro
+    for id_pnc in new_linked_pncs:
+        if id_pnc in registro_gral:
+            return Response(data={
+                "Error": f"Estructura de reporte_<ID> no valida. Id '{id_pnc}' no existe en el servidor"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
+    return True
+
+
+def checkPNC(pnc):
+    atributos_pnc = {
+            "numeroPNC": {"dataClass": int},
+            "folio": {"dataClass": str},
+            "fechaRegistro": {"dataClass": str},
+            "especIncumplida": {"dataClass": str},
+            "accionImplantada": {"dataClass": str},
+            "isEliminaPNC": {"dataClass": bool}
+            }
+
+    new_registro_pnc_keys = pnc.keys()
+    if len(new_registro_pnc_keys) != 6:
+        return Response(data={
+            "Error": "Estructura de pnc_<ID> no valida. Elementos no concuerdan"
+            },
+            status=status.HTTP_400_BAD_REQUEST)
+
+    for atributo_pnc in atributos_pnc:
+        try:
+            valor_atributo = pnc[atributo_pnc]
+        except KeyError:
+            return Response(data={
+                "Error": f"Estructura de pnc_<ID> no valida. El atributo '{atributo_pnc}' no existe en el nuevo pnc"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+
+        if type(valor_atributo) is not atributos_pnc[atributo_pnc]["dataClass"]:
+            return Response(data={
+                "Error": f"Estructura de pnc_<ID> no valida. El valor relacionado con el atributo '{valor_atributo}' no es de tipo '{atributos_pnc[atributo_pnc]['dataClass']}'"
+                },
+                status=status.HTTP_400_BAD_REQUEST)
+    return True
+
+
+def validarDatos(datos, registro_gral_pnc):
+    check_datos = checkReceivedData(datos)
+    if type(check_datos) is Response:
+        return check_datos
+
+    lastPNCID, registro = check_datos
     registro_keys = registro.keys()
     rgx_id_reporte = r"(?:^reporte_[0-9]+$)"
     rgx_id_pnc = r"(?:^pnc_[0-9]+$)"
@@ -53,20 +174,22 @@ def validarDatos(datos, registro_gral_pnc):
             },
             status=status.HTTP_400_BAD_REQUEST)
 
+    # TODO: Cambiar las variables has_ por contadores
+    # TODO: Cambiar los registro_..._id por arreglos y hacer que agregue los
+    #       matches de los rgx_id_...correspondientes
     has_reportes_registrados = False
     has_reporte_id = False
     has_pnc_id = False
     registro_reporte_id = ""
     registro_pnc_id = ""
     for key in registro_keys:
-        if len(registro_keys) == 3:
-            if key == "reportesRegistrados":
-                if has_reportes_registrados:
-                    return Response(data={
-                        "Error": "Estructura de registro no valida. Elemento reportesRegistrados repetido"
-                        },
-                        status=status.HTTP_400_BAD_REQUEST)
-                has_reportes_registrados = True
+        if len(registro_keys) == 3 and key == "reportesRegistrados":
+            if has_reportes_registrados:
+                return Response(data={
+                    "Error": "Estructura de registro no valida. Elemento reportesRegistrados repetido"
+                    },
+                    status=status.HTTP_400_BAD_REQUEST)
+            has_reportes_registrados = True
 
         if re.match(rgx_id_reporte, key) is not None:
             if has_reporte_id:
@@ -109,121 +232,34 @@ def validarDatos(datos, registro_gral_pnc):
 
     if len(registro_keys) == 3:
         new_reportes_registrados = registro["reportesRegistrados"]
-        new_reportes_registrados_keys = new_reportes_registrados.keys()
-        if len(new_reportes_registrados_keys) != 1:
-            return Response(data={
-                "Error": "Estructura de reportesRegistrados no valida. Más elementos de los esperados"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        if "idsReportes" not in new_reportes_registrados_keys:
-            return Response(data={
-                "Error": "Estructura de reportesRegistrados no valida. No existe el elemento 'idsReportes'"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        new_ids_reportes = new_reportes_registrados["idsReportes"]
-        if type(new_ids_reportes) is not list:
-            return Response(data={
-                "Error": "Estructura de reportesRegistrados no valida. Valor relacionado a 'idsReportes' no es de tipo 'arreglo'"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        # El ultimo id agregado en idsReportes debe ser el del nuevo reporte
-        if registro_reporte_id != new_ids_reportes[-1]:
-            return Response(data={
-                "Error": "Estructura de reportesRegistrados no valida. Id del nuevo reporte no concuerda"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        # Todos los ids dentro de idsReportes menos el ultimo son de reportes
-        # existentes
-        new_ids_reportes = new_ids_reportes[:-1]
-        # Comprobando si los ids pasados por parametro realmente existen en el registro
-        for id_reporte in new_ids_reportes:
-            try:
-                registro_gral_pnc["registro"][id_reporte]
-            except KeyError:
-                return Response(data={
-                    "Error": f"Estructura de reportesRegistrados no valida. Id '{id_reporte}' no existe en el servidor"
-                    },
-                    status=status.HTTP_400_BAD_REQUEST)
-
-    registro_reporte = registro[registro_reporte_id]
-    new_registro_reporte_key = registro_reporte.keys()
-    if len(new_registro_reporte_key) != 1:
-        return Response(data={
-            "Error": "Estructura de reporte_<ID> no valida. Más elementos de los esperados"
-            },
-            status=status.HTTP_400_BAD_REQUEST)
-
-    if "linkedPNCs" not in new_registro_reporte_key:
-        return Response(data={
-            "Error": "Estructura de reporte_<ID> no valida. No existe el elemento 'linkedPNCs'"
-            },
-            status=status.HTTP_400_BAD_REQUEST)
-
-    new_linked_pncs = registro_reporte["linkedPNCs"]
-    if type(new_linked_pncs) is not list:
-        return Response(data={
-            "Error": "Estructura de reporte_<ID> no valida. Valor relacionado a 'linkedPNCs' no es de tipo 'arreglo'"
-            },
-            status=status.HTTP_400_BAD_REQUEST)
+        check_repo_registro = checkReportesRegistrados(registro_reporte_id,
+                                                       new_reportes_registrados,
+                                                       registro_gral_pnc["registro"]["reportesRegistrados"]["idsReportes"])
+        if type(check_repo_registro) is Response:
+            return check_repo_registro
 
     # Si unicamente se reciben 2 elementos (reporte_<ID> y pnc_<ID>) significa
     # que existe ya una referencia a dicho reporte en el registro general
     if len(registro_keys) == 2:
         print(f"\n\n\n\tRegistro Reporte ID: {registro_reporte_id}")
         print(f"\n\n\n\tRegistro General: {registro_gral_pnc}")
-        try:
-            registro_gral_pnc["registro"][registro_reporte_id]
-        except KeyError:
+        if registro_reporte_id in registro_gral_pnc["registro"]:
             return Response(data={
                 "Error": "Estructura de reporte_<ID> no valida. El reporte que se esta actualizando no existe en el registro general"
                 },
                 status=status.HTTP_400_BAD_REQUEST)
 
-    # El último id agregado en linkedPNCs debe ser el nuevo pnc
-    if registro_pnc_id != new_linked_pncs[-1]:
-        return Response(data={
-            "Error": "Estructura de reporte_<ID> no valida. Id del nuevo PNC no concuerda"
-            },
-            status=status.HTTP_400_BAD_REQUEST)
-
-    # Todos los ids dentro de linkedPNCs menos el ultimo son de pncs existentes
-    new_linked_pncs = new_linked_pncs[:-1]
-    # Comprobando si los ids pasados por parametro realmente existen en el registro
-    for id_pnc in new_linked_pncs:
-        try:
-            registro_gral_pnc["registro"][id_pnc]
-        except KeyError:
-            return Response(data={
-                "Error": f"Estructura de reporte_<ID> no valida. Id '{id_pnc}' no existe en el servidor"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
+    registro_reporte = registro[registro_reporte_id]
+    check_reporte = checkReporte(registro_pnc_id,
+                                 registro_reporte,
+                                 registro_gral_pnc["registro"])
+    if type(check_reporte) is Response:
+        return check_reporte
 
     new_registro_pnc = registro[registro_pnc_id]
-    new_registro_pnc_keys = new_registro_pnc.keys()
-    if len(new_registro_pnc_keys) != 6:
-        return Response(data={
-            "Error": "Estructura de pnc_<ID> no valida. Elementos no concuerdan"
-            },
-            status=status.HTTP_400_BAD_REQUEST)
-
-    for atributo_pnc in atributos_pnc:
-        try:
-            valor_atributo = new_registro_pnc[atributo_pnc]
-        except KeyError:
-            return Response(data={
-                "Error": f"Estructura de pnc_<ID> no valida. El atributo '{atributo_pnc}' no existe en el nuevo pnc"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
-
-        if type(valor_atributo) is not atributos_pnc[atributo_pnc]["dataClass"]:
-            return Response(data={
-                "Error": f"Estructura de pnc_<ID> no valida. El valor relacionado con el atributo '{valor_atributo}' no es de tipo '{atributos_pnc[atributo_pnc]['dataClass']}'"
-                },
-                status=status.HTTP_400_BAD_REQUEST)
+    check_pnc = checkPNC(new_registro_pnc)
+    if type(check_pnc) is Response:
+        return check_pnc
 
     return (lastPNCID,
             registro,
